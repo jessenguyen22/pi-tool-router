54db2d0064329f3e4ceec8d2787ebe48d5eccecd
jessenguyen22 <official.jessenguyen@gmail.com>
2026-04-26 18:03:59 +0700

    fix: migrate ESLint config to flat style (v9)
    - Remove deprecated .eslintrc.js
    - Add new eslint.config.js with flat config format
    - Update lint script for new ESLint
    - Fix 2 warnings (unused variables)

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['dist/', 'node_modules/', 'coverage/', '*.js'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
